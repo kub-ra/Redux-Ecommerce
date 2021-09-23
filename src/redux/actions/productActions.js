d13a6cba6354a660fb19a3468130e992b1eb0cdc
Kübra Menziletoğlu <73390061+kub-ra@users.noreply.github.com>
2021-09-23 17:55:30 +0300

    added redux saveProduct

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,16 +1,46 @@
 import * as actionTypes from './actionTypes'
 
 
-export function getProductSucces(products){
-    return {type:actionTypes.GET_PRODUCT_SUCCESS,payload:products}
+export function getProductSucces(products) {
+    return { type: actionTypes.GET_PRODUCT_SUCCESS, payload: products }
 }
-export function getProducts(categoryId){
-    return function(dispatch){
-        let url=" http://localhost:3000/products"
-        if(categoryId){
-            url=url+"?categoryId="+categoryId
+export function createProductSuccess(product) {
+    return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product }
+}
+export function updateProductSuccess(product) {
+    return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product }
+}
+export async function handleResponse(responce){
+    if(responce.ok){
+        return responce.json();
+    }
+    const error=await responce.text()
+    throw new Error(error)
+}
+export function handleError(error){
+    console.log("Bir hata oluştu")
+    throw error;
+}
+export function saveProductApi(product) {
+    return fetch(" http://localhost:3000/products/" + (product.id || ""), {
+        method: product.id ? "PUT" : "POST", headers: { "content-type": "aplication/json" }, body: JSON.stringify(product)
+    }).then(handleResponse).catch(handleError);
+}
+export function saveProduct(product) {
+    return function (dispatch) {
+        return saveProductApi(product).then(savedproduct => {
+            product.id ? dispatch(updateProductSuccess(savedproduct)) : dispatch(createProductSuccess(savedproduct));
+         }).catch(error=>{throw error})
+    }
+
+}
+export function getProducts(categoryId) {
+    return function (dispatch) {
+        let url = " http://localhost:3000/products"
+        if (categoryId) {
+            url = url + "?categoryId=" + categoryId
         }
-        return fetch(url).then(response=>response.json())
-        .then(result=>dispatch(getProductSucces(result)))
+        return fetch(url).then(response => response.json())
+            .then(result => dispatch(getProductSucces(result)))
     }
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,8 +3,9 @@ import changecategoryReducer from './changecategoryReducer'
 import categoryListReducer from './categoryListReducer'
 import productListReducer from './productListReducer'
 import cartReducer from './cartReducer'
+import saveProductReducer from './saveProductReducer'
 
 const rootReducer=combineReducers({
-    changecategoryReducer,categoryListReducer,productListReducer,cartReducer
+    changecategoryReducer,categoryListReducer,productListReducer,cartReducer,saveProductReducer
 })
 export default rootReducer;
